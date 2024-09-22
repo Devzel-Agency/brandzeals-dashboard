48b9222340fabd9a4773e49ee4f97e584bd11b7b
shaik-moazzam <shaikmoazzam1@gmail.com>
2024-09-23 03:22:34 +0530

    push

--- sections/dashboard/dashboard.js
+++ sections/dashboard/dashboard.js
@@ -1,62 +1,118 @@
-"use client"
-import getDashboarddata from '@/api/getDashboardData'
-import Dashcard from '@/components/dashcard'
-import Graph from '@/components/graph'
-import Padding from '@/components/padding'
-import React, { useEffect, useState } from 'react'
+"use client";
+import getDashboarddata from "@/api/getDashboardData";
+import Dashcard from "@/components/dashcard";
+import Graph from "@/components/graph";
+import Padding from "@/components/padding";
+import clsx from "clsx";
+import React, { useEffect, useState } from "react";
 
 const Dashboard = () => {
-    const [data,setdata]=useState()
-    const [loading,setloading]=useState()
-    useEffect(()=>{
-        const getdata=async()=>{
-            const data=await getDashboarddata()
-            if(!data){
-                return
-            }
-            console.log(data);
-            setdata(data)
-        }
-        getdata()
-    },[])
-    return (
-        <div className=' py-10  bg-[#FBFBFB]'>
-            <Padding>
-                <div className=' lg:flex  gap-5 ' >
-                    <div className=' grid gap-5 lg:w-[45%] xl:w-[35%] '>
-                        <Dashcard heading={"Total Influncer"} no={data?.onboardinfluencer} tag={"Influencers onboarded"}/>
-                        <Dashcard heading={"Total Brands"} no={data?.onboardbrands} tag={"Brands onboarded"}  />
-                    </div>
-                    <div className=' lg:w-[55%] xl:w-[65%] '>
-                     <Graph/>
-                    </div>
-                </div>
-                <div>
-                    <div className=' flex justify-between py-4 pt-10 text-[1.5rem] font-Satoshi  font-bold  '>Recent Brands <div className=' font-Matter  font-medium cursor-pointer text-[0.9rem] '>View all</div> 
-                    </div>
-                    <div className=' bg-[#FFFFFF]  border-[#F1F1F1] border rounded-xl '>
-                        <div className=' grid grid-cols-6 px-5 py-3.5 '>
-                            <div>S.No</div>
-                            <div>Name</div>
-                            <div>Email</div>
-                            <div>Brand Name</div>
-                            <div>Phone Number</div>
-                            <div>Date</div>
-                        </div>
-
-                        <div className=' bg-[#F9FAFB] grid grid-cols-6 px-5 py-3.5 '>
-                            <div>1</div>
-                            <div>Abdul Aziz</div>
-                            <div className=' w-full truncate  lowercase'>Mohammed@gmail.com</div>
-                            <div>Abdul Aziz </div>
-                            <div>7794966120</div>
-                            <div>24/2/2024</div>
-                        </div>
-                    </div>
-                </div>
-            </Padding>
+  const [data, setdata] = useState();
+  const [loading, setloading] = useState();
+  useEffect(() => {
+    const getdata = async () => {
+      const data = await getDashboarddata();
+      if (!data) {
+        return;
+      }
+      console.log(data);
+      setdata(data);
+    };
+    getdata();
+  }, []);
+  return (
+    <div className=" py-10  bg-[#FBFBFB]">
+      <Padding>
+        <div className=" lg:flex  gap-5 ">
+          <div className=" grid gap-5 lg:w-[45%] xl:w-[35%] ">
+            <Dashcard
+              heading={"Total Influncer"}
+              no={data?.onboardinfluencer}
+              tag={"Influencers onboarded"}
+            />
+            <Dashcard
+              heading={"Total Brands"}
+              no={data?.onboardbrands}
+              tag={"Brands onboarded"}
+            />
+          </div>
+          <div className=" lg:w-[55%] xl:w-[65%] ">
+            <Graph />
+          </div>
         </div>
-    )
-}
+        <div>
+          <div className=" flex justify-between py-4 pt-10 text-[1.5rem] font-Satoshi  font-bold  ">
+            Recent Brands{" "}
+            <div className=" font-Matter  font-medium cursor-pointer text-[0.9rem] ">
+              View all
+            </div>
+          </div>
+          <div className=" bg-[#FFFFFF]  border-[#F1F1F1] border rounded-xl ">
+            <div className=" font-Matter font-medium grid grid-cols-6 gap-6 px-5 py-3.5 ">
+              <div>S.No</div>
+              <div>Name</div>
+              <div>Email</div>
+              <div>Brand Name</div>
+              <div>Phone Number</div>
+              <div>Date</div>
+            </div>
+            {data?.recentbrands?.map((brand, idx) => (
+              <div
+                className={clsx(
+                  "  grid grid-cols-6 font-Matter gap-6 px-5 py-3.5 ",
+                  idx % 2 == 0 ? "bg-[#F9FAFB]" : " bg-white "
+                )}
+              >
+                <div>{idx + 1}</div>
+                <div>{brand?.name}</div>
+                <div className=" w-full truncate  lowercase">
+                  {brand?.email}
+                </div>
+                <div>{brand?.brand}</div>
+                <div>{brand?.phone}</div>
+                <div>{brand?.createdAt}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div>
+          <div className=" flex justify-between py-4 pt-20 text-[1.5rem] font-Satoshi  font-bold  ">
+            Recent Influencers
+            <div className=" font-Matter  font-medium cursor-pointer text-[0.9rem] ">
+              View all
+            </div>
+          </div>
+          <div className=" bg-[#FFFFFF]  border-[#F1F1F1] border rounded-xl ">
+            <div className=" font-Matter font-medium grid grid-cols-6 gap-6 px-5 py-3.5 ">
+              <div>S.No</div>
+              <div>Name</div>
+              <div>Email</div>
+              <div>Brand Name</div>
+              <div>Phone Number</div>
+              <div>Date</div>
+            </div>
+            {data?.recentinfluencers?.map((brand, idx) => (
+              <div
+                className={clsx(
+                  "  grid grid-cols-6 font-Matter gap-6 px-5 py-3.5 ",
+                  idx % 2 == 0 ? "bg-[#F9FAFB]" : " bg-white "
+                )}
+              >
+                <div>{idx + 1}</div>
+                <div>{brand?.name}</div>
+                <div className=" w-full truncate  lowercase">
+                  {brand?.email}
+                </div>
+                <div>{brand?.brand}</div>
+                <div>{brand?.phone}</div>
+                <div>{brand?.createdAt}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </Padding>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
