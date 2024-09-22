321c719950f3ec8040394dc54422836753d1d63c
reach2mohammed <mustafashareef@devsed.com>
2024-09-23 02:53:18 +0530

    done

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,9 +1,10 @@
+import Dashboard from '@/sections/dashboard/dashboard'
 import React from 'react'
 
 const Page = () => {
   return (
     <div>
-      
+      <Dashboard/>
     </div>
   )
 }
--- sections/dashboard/dashboard.js
+++ sections/dashboard/dashboard.js
@@ -23,15 +23,15 @@ const Dashboard = () => {
             <Padding>
                 <div className=' lg:flex  gap-5 ' >
                     <div className=' grid gap-5 lg:w-[45%] xl:w-[35%] '>
-                        <Dashcard heading={"Total influncer"} no={data?.onboardinfluencer} tag={"Influencers onboarded"}/>
-                        <Dashcard heading={"Total brands"} no={data?.onboardbrands} tag={"Brands onboarded"}  />
+                        <Dashcard heading={"Total Influncer"} no={data?.onboardinfluencer} tag={"Influencers onboarded"}/>
+                        <Dashcard heading={"Total Brands"} no={data?.onboardbrands} tag={"Brands onboarded"}  />
                     </div>
                     <div className=' lg:w-[55%] xl:w-[65%] '>
                         Graph
                     </div>
                 </div>
                 <div>
-                    <div className=' flex justify-between py-4 pt-10 text-[1.5rem] font-Satoshi font-medium  '>Recent Brands <div className=' font-Matter font-normal text-[0.9rem] '>View all</div> 
+                    <div className=' flex justify-between py-4 pt-10 text-[1.5rem] font-Satoshi  font-bold  '>Recent Brands <div className=' font-Matter  font-medium cursor-pointer text-[0.9rem] '>View all</div> 
                     </div>
                     <div className=' bg-[#FFFFFF]  border-[#F1F1F1] border rounded-xl '>
                         <div className=' grid grid-cols-6 px-5 py-3.5 '>
@@ -46,7 +46,7 @@ const Dashboard = () => {
                         <div className=' bg-[#F9FAFB] grid grid-cols-6 px-5 py-3.5 '>
                             <div>1</div>
                             <div>Abdul Aziz</div>
-                            <div className=' w-full truncate  lowercase'>Mohammedd@gmail.com</div>
+                            <div className=' w-full truncate  lowercase'>Mohammed@gmail.com</div>
                             <div>Abdul Aziz </div>
                             <div>7794966120</div>
                             <div>24/2/2024</div>
