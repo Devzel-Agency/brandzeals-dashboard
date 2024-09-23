c608be4b234ecce7ad7abd7f4f0c5e1187dd73a3
reach2mohammed <mustafashareef@devsed.com>
2024-09-23 18:42:32 +0530

    done

--- sections/dashboard/brands.js
+++ sections/dashboard/brands.js
@@ -2,6 +2,7 @@
 import getbrands from "@/api/getbrands";
 import Padding from "@/components/padding";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { convertTimestampToDate } from "@/utils/convertTimestampToDate";
 import clsx from "clsx";
 import React, { useEffect, useState } from "react";
 
@@ -35,7 +36,6 @@ const Brands = () => {
         {data?.map((brand, idx) => (
           <Sheet className=" w-full text-start">
             <SheetTrigger className="  w-full text-start">
-
               <div
                 className={clsx(
                   "  grid grid-cols-5 font-Matter gap-6 px-5 py-3.5 ",
@@ -44,48 +44,78 @@ const Brands = () => {
               >
                 <div>{idx + 1}</div>
                 <div className=" w-full truncate ">{brand?.brand}</div>
-                <div className=" w-full truncate  lowercase">{brand?.email}</div>
+                <div className=" w-full truncate  lowercase">
+                  {brand?.email}
+                </div>
                 <div>{brand?.phone}</div>
                 <div>{brand?.createdAt}</div>
               </div>
             </SheetTrigger>
-            <SheetContent className=" z-[5000000000000]">
-              <div className=" py-16 px-6  flex flex-col gap-5 right-0 top-0  w-full bg-white  h-full ">
+            <SheetContent className=" p-6 py-10 z-[5000000000000]">
+              <div className=" h-max max-h-full rounded-xl  p-6  border-[1px] border-[#F1F1F1]   flex flex-col gap-5 right-0 top-0  w-full bg-white  ">
                 <div className=" flex gap-4 text-[1.1rem] ">
                   <div className=" font-Satoshi font-medium text-[#606061] ">
                     Brand Name :
                   </div>
-                  <div className=" font-Matter font-medium   ">Brandzeals</div>
+                  <div className=" font-Matter font-medium   ">
+                    {brand.brand}
+                  </div>
                 </div>
                 <div className=" flex gap-4 text-[1.1rem] ">
-                  <div className=" font-Satoshi font-medium text-[#606061] ">Email :</div>
+                  <div className=" font-Satoshi font-medium text-[#606061] ">
+                    Owner Name :
+                  </div>
                   <div className=" font-Matter font-medium   ">
-                    mohammedb4u2@gmail.com
+                    {brand.name}
+                  </div>
+                </div>
+                <div className=" flex gap-4 text-[1.1rem] ">
+                  <div className=" font-Satoshi font-medium text-[#606061] ">
+                    Email :
+                  </div>
+                  <div className=" font-Matter font-medium   ">
+                    {brand.email}
                   </div>
                 </div>
                 <div className=" flex gap-4 text-[1.1rem] ">
                   <div className=" font-Satoshi font-medium text-[#606061] ">
                     Phone Number :
                   </div>
-                  <div className=" font-Matter font-medium   ">+91 7794966120</div>
+                  <div className=" font-Matter font-medium   ">
+                    {brand.phone}
+                  </div>
                 </div>
                 <div className=" flex gap-4 text-[1.1rem] ">
                   <div className=" font-Satoshi font-medium text-[#606061] ">
                     Onboarding Date :
                   </div>
-                  <div className=" font-Matter font-medium   ">23/9/10</div>
+                  <div className=" font-Matter font-medium   ">
+                    {convertTimestampToDate(brand.updatedAt)}
+                  </div>
                 </div>
                 <div className=" flex gap-4 text-[1.1rem] ">
                   <div className=" font-Satoshi font-medium text-[#606061] ">
-                    Person Incharge
+                    Description
                   </div>
-                  <div className=" font-Matter font-medium   ">Mustafa</div>
+                  <div className=" font-Matter font-medium   ">
+                    {brand.about}
+                  </div>
                 </div>
-                <div className=" flex gap-4 text-[1.1rem] ">
+                <div className="  flex flex-col gap-4 text-[1.1rem] ">
                   <div className=" font-Satoshi font-medium text-[#606061] ">
-                    Platforms :
+                   Requested Platforms :
                   </div>
-                  <div className=" font-Matter font-medium   ">Platforms</div>
+                  <div className="   grid-cols-3 grid gap-4 font-Matter font-medium   ">
+                    {brand.socials.map((img) => (
+                      <div className=" h-full w-full flex justify-center items-center">
+                      <img
+                        src={`/images/${img}.svg`}
+                        alt=""
+                        srcset=""
+                      />
+                      </div>
+                    ))}
+                  </div>
                 </div>
               </div>
             </SheetContent>
