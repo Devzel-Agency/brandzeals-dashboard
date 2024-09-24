26a696011f5ec9229689b9946aeb30a97d6c9f5e
shaik-moazzam <shaikmoazzam1@gmail.com>
2024-09-24 22:17:12 +0530

    push

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,18 +1,13 @@
-
-import Dashboardnavbar from '@/components/dashboardnavbar'
-import React from 'react'
+import Dashboardnavbar from "@/components/dashboardnavbar";
+import React from "react";
 
 const Layout = ({ children }) => {
-    return (
-        <div className=' flex flex-col relative h-[100svh] xl:h-[100vh] '>
-            <Dashboardnavbar />
-            <div className=' h-max  flex-grow  w-full pb-20 md:pb-0'>
+  return (
+    <div className=" flex flex-col relative h-[100svh] xl:h-[100vh] ">
+      <Dashboardnavbar />
+      <div className=" h-max  flex-grow  w-full pb-20 md:pb-0">{children}</div>
+    </div>
+  );
+};
 
-                {children}
-            </div>
-
-        </div>
-    )
-}
-
-export default Layout
+export default Layout;
